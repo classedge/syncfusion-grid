947bd0a3fec63eb021bcac54929c58a4fced1992
TataClassEdge <app.tataclassedge@gmail.com>
2020-05-09 23:48:29 +0530

    done 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<ejs-grid [dataSource]="data"></ejs-grid>',
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-grid-app';
+  public data: object[] = [
+    {
+
+      id:2,
+      name: 'This is prod name 2'
+    },
+    {
+
+      id:3,
+      name: 'This is prod name 3'
+    }
+  ]
 }
